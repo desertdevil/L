3cc53190c6500a7ded9105c8e169d2800cd7d0cd
Chuck Dries <chuck@chuckdries.com>
2018-09-19 18:47:20 -0700

    add some code

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-console.log('hello, world!');
-console.log('ni hao');
+const express = require('express');
+const app = express();
+const sqlite = require('sqlite');
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'twig');
+
+app.use(require('body-parser').urlencoded({ extended: true }));
+
+const dbPromise = sqlite.open('./data.sqlite');
+
+app.get('/', async function (req, res){
+  res.render('index');
+});
+
+
+app.listen(3000);
